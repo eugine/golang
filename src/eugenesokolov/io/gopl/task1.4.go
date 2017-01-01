3b088fb7ca809e29b4d316e3015274610b7da0a3
Eugene Sokolov <eugine_s@ukr.net>
2017-01-01 20:37:48 +0100

    Implemented task1.4

--- src/eugenesokolov/io/gopl/task1.4.go
+++ src/eugenesokolov/io/gopl/task1.4.go
@@ -8,9 +8,11 @@ import (
 
 func main()  {
     counts := make(map[string]int)
+    names := make(map[string][]string)
+
     files := os.Args[1:]
     if (len(files) == 0) {
-        countLines(os.Stdin, counts)
+        countLines(os.Stdin, counts, names)
     } else {
         for _, arg := range files {
             f, err := os.Open(arg)
@@ -18,20 +20,21 @@ func main()  {
                 fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
                 continue
             }
-            countLines(f, counts)
+            countLines(f, counts, names)
             f.Close()
         }
         for line, n := range counts {
             if n > 1 {
-                fmt.Printf("%d\t%s\n", n, line)
+                fmt.Printf("%d\t%s\t%s\n", n, line, names[line])
             }
         }
     }
 }
 
-func countLines(f *os.File, counts map[string]int)  {
+func countLines(f *os.File, counts map[string]int, names map[string][]string)  {
     input := bufio.NewScanner(f)
     for input.Scan() {
         counts[input.Text()]++
+        names[input.Text()] = append(names[input.Text()], f.Name())
     }
 }
